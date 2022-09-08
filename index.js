e23a97ed9e13a80e9edb2c95ad1857815db587dc
Brandon Castro <castroyou999@gmail.com>
2022-09-08 15:07:27 -0400

    commit tree d08b168033ef4bd07f4b5dc39a744c4711becad8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+};
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+};
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return bestCustomer
+};
+const leastFavoriteCustomer = "someguy";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "someotherguy";
+    return leastFavoriteCustomer;
+};
